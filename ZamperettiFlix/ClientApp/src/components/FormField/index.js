7edf705ed4804a3d748c2e9cae7c4334d7f23fcb
Marcio Zamperetti <marcioff6@hotmail.com>
2020-07-29 21:32:11 -0300

    cadastro categoria wip

---
+++ ZamperettiFlix/ClientApp/src/components/FormField/index.js
@@ -0,0 +1,23 @@
+﻿import React from "react";
+
+function FormField({ type, label, name, value, onChange}) {
+    return (
+
+        <div>
+            <label>
+                {label}:
+                    <input
+                    type={type}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+        
+        )
+
+}
+
+
+export default FormField
--- ZamperettiFlix/ClientApp/src/pages/cadastro/categoria/index.js
+++ ZamperettiFlix/ClientApp/src/pages/cadastro/categoria/index.js
@@ -1,24 +1,79 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const [categoria, setCategorias] = useState([]);
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '#000'
+    }
+    const [valores, setValores] = useState(valoresIniciais);
+
+    function setValor(nome, valor) {
+        setValores({
+            ...valores,
+            [nome]: valor
+        })
+    }
+
+    function handleChange(infoEvento) {
+        setValor(
+            infoEvento.target.getAttribute('name'),
+            infoEvento.target.value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            <form>
+            <h1>Cadastro de Categoria: {valores.nome}</h1>
 
-                <label>
-                    Nome da Categoria:
-          <input
-                        type="text"
-                    />
-                </label>
-
+            <form onSubmit={function handleSubmit(infoEvento) {
+                infoEvento.preventDefault();
+                setCategorias([
+                    ...categoria,
+                    valores
+                ])
+            }}>
+                <FormField
+                    label="Nome da Categoria"
+                    value={valores.nome}
+                    name="nome"
+                    type="text"
+                    onChange={handleChange}
+                />
+                <FormField
+                    label="Nome da Descerição"
+                    value={valores.descricao}
+                    name="descricao"
+                    type="text"
+                    onChange={handleChange}
+                />
+                <FormField
+                    label="Cor"
+                    value={valores.color}
+                    name="cor"
+                    type="color"
+                    onChange={handleChange}
+                />
                 <button>
                     Cadastrar
-        </button>
+                 </button>
             </form>
+
+            <ul>
+                {categoria.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                    {categoria.nome}
+                    </li>
+                )
+
+                })}
+            </ul>
+
 
             <Link to="/">
                 ir para Home
